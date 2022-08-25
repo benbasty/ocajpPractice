ffc64be88b26e782aebb76d96dbd82ed1e44b3e6
benbasty <benbasty@yahoo.fr>
2022-08-26 02:52:32 +0800

    ocajp practice 2

---
+++ src/com/projects/DataList.java
@@ -0,0 +1,15 @@
+package com.projects;
+
+public class DataList {
+    public static void main(String[] args) {
+        String[] dataList = {"x","y","z"};
+        for(String dataElement: dataList) {
+            int innerCounter = 0;
+            while (innerCounter < dataList.length) {
+                System.out.println(dataElement + "," + innerCounter);
+                innerCounter++;
+            }
+        }
+    }
+
+}
---
+++ src/com/projects/TestClassLoopConstructs.java
@@ -0,0 +1,9 @@
+package com.projects;
+
+public class TestClassLoopConstructs {
+    public static void main(String[] args) {
+        //while(int k = 5; k<7){
+         //   System.out.println(k++);
+       // }
+    }
+}
